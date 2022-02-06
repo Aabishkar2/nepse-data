1133eb4e66747e54563ffc7c89446da5170562d3
Aabishkar Wagle <aabishkar2@gmail.com>
2022-02-06 23:34:46 +0545

    Add code for requesting data

---
+++ src/config/headers.py
@@ -0,0 +1,13 @@
+headers = {
+    'Connection': 'keep-alive',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+    'Accept': 'application/json, text/javascript, /; q=0.01',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://www.sharesansar.com/company/cbl',
+    'Accept-Language': 'en-US,en;q=0.9,ne;q=0.8',
+}
---
+++ src/config/url.py
@@ -0,0 +1,1 @@
+url = 'https://www.sharesansar.com/company-price-history'
