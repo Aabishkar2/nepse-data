439e23156a47207b79dc77a910c68d6a2c175a3e
Aabishkar Wagle <aabishkar2@gmail.com>
2022-02-21 21:11:28 +0545

    Update requirements

--- src/dailyDataScrapper.py
+++ src/dailyDataScrapper.py
@@ -11,7 +11,7 @@ html = requests.get(dailyPriceUrl).text
 bs = BeautifulSoup(html, "lxml")
 
 # today date in yyyy-mm-dd format
-today = bs.find('span',{'class':'text-org'}).text
+today = bs.find("span", {"class": "text-org"}).text
 
 # get html tables
 tables = pd.read_html(html)
@@ -25,22 +25,22 @@ for file in fileDir.glob("*.csv"):
     existingDf = pd.read_csv(file)
     lastRow = existingDf.iloc[-1]
     lastDate = lastRow["published_date"]
-    if(str(lastDate) != str(today))
+    if str(lastDate) != str(today):
         symbol = str(file).split(".")[2].split("/")[-1]
         data = dataTable.loc[dataTable["Symbol"] == symbol]
         status = getStatus(float(data["Open"]), float(data["Close"]))
-        dataRow = [[
-            today,
-            float(data["Open"]),
-            float(data["High"]),
-            float(data["Low"]),
-            float(data["Close"]),
-            float(data["Diff %"]),
-            float(data["Vol"]),
-            float(data["Turnover"]),
-            status,
-        ]]
-        dataframe = pd.DataFrame(dataRow) 
-        dataframe.to_csv(file, mode='a', header=False, index=False)
-
-    
+        dataRow = [
+            [
+                today,
+                float(data["Open"]),
+                float(data["High"]),
+                float(data["Low"]),
+                float(data["Close"]),
+                float(data["Diff %"]),
+                float(data["Vol"]),
+                float(data["Turnover"]),
+                status,
+            ]
+        ]
+        dataframe = pd.DataFrame(dataRow)
+        dataframe.to_csv(file, mode="a", header=False, index=False)
